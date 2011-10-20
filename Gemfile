7c8fbf3a48310474025479d8cb0506ed92db6a46
Lucas Souza <lasouza@sp.r7.com>
2011-10-20 14:49:52 -0200

    Preparing application to use rails

--- Gemfile
+++ Gemfile
@@ -3,6 +3,11 @@ source "http://rubygems.org"
 # Specify your gem's dependencies in will_paginate_mongoid.gemspec
 gemspec
 
+group :development do
+  gem "rails", "3.0.0"
+end
+
 group :test do
-	gem "rspec", "2.7.0"
+  gem "rspec", "2.7.0"
+  gem "rspec-rails", "2.7.0"
 end
---
+++ spec/fake_app.rb
@@ -0,0 +1,8 @@
+require 'action_controller/railtie'
+require 'action_view/railtie'
+
+app = Class.new(Rails::Application)
+app.config.secret_token = "3b7cd727ee24e8444053437c36cc66c4"
+app.config.session_store :cookie_store, :key => "_myapp_session"
+app.config.active_support.deprecation = :log
+app.initialize!
