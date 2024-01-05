80d1f2630a2032b14ccd8e12885a49bcff003961
RobL <contact@robl.me>
2024-01-05 10:13:48 +0000

    Supporting Mongoid 8.x / WillPaginate 4.0.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,19 +4,19 @@ require 'will_paginate'
 require 'will_paginate_mongoid'
 require File.join(File.dirname(__FILE__), 'fake_app')
 require 'rspec/rails'
-
+require 'rspec/its'
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
   config.mock_with :rspec
   config.before :each do
-    Mongoid.master.collections.select do |collection|
+    Mongoid::Clients.default.collections.select do |collection|
       collection.name !~ /system/
     end.each(&:drop)
   end
 end
 
 Mongoid.configure do |config|
-  config.master = Mongo::Connection.new.db("will_paginate_mongoid_test")
+  config.connect_to("will_paginate_mongoid_test")
 end
